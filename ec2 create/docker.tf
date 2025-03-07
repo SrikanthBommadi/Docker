19620725c1f307e25f4a1fd9519249831b3a7a38
SrikanthBommadi <bommadisrikanth1@gmail.com>
2025-03-07 07:16:30 -0500

    last

--- ec2 create/docker.tf
+++ ec2 create/docker.tf
@@ -10,27 +10,28 @@ resource "aws_instance" "docker" {
         }
     )
 
-root_block_device {
-    volume_size = 50  # Set root volume size to 50GB
-    volume_type = "gp3"  # Use gp3 for better performance (optional)
-  }
+# root_block_device {
+#     volume_size = 50  # Set root volume size to 50GB
+#     volume_type = "gp3"  # Use gp3 for better performance (optional)
+#   }
 
- provisioner "remote-exec" {
-    inline = [
-      "sudo dnf -y install dnf-plugins-core",
-      "sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo",
-      "sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin",
-      "sudo systemctl enable --now docker",
-      "sudo systemctl start docker",
-      "sudo usermod -aG docker ec2-user"
-    ]
-  }
-  connection {
-    type        = "ssh"
-    user        = "ec2-user"
-    password    = "DevOps321"
-    host        = self.public_ip
-  }
+#  provisioner "remote-exec" {
+#     inline = [
+#       "sudo dnf -y install dnf-plugins-core",
+#       "sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo",
+#       "sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin",
+#       "sudo systemctl enable --now docker",
+#       "sudo systemctl start docker",
+#       "sudo usermod -aG docker ec2-user"
+#     ]
+#   }
+#   connection {
+#     type        = "ssh"
+#     user        = "ec2-user"
+#     password    = "DevOps321"
+#     host        = self.public_ip
+#   }
+  user_data = file("docker.sh")
 }
 
 
